6c469d36259ab3beddf51f4556a1dc75d37baf0e
goodeeit <gt_min@naver.com>
2023-07-13 17:11:06 +0900

    20230713

--- 04_Control_Ex/src/ex01_branch/MainWrapper.java
+++ 04_Control_Ex/src/ex01_branch/MainWrapper.java
@@ -24,7 +24,24 @@ public class MainWrapper {
     // 점수에 따른 학점(switch문)
     int score = 100;
     char grade;  // 'A', 'B', 'C', 'D', 'F'
-    
+    switch(score / 10) {
+    case 10:
+    case 9:
+      grade = 'A';
+      break;
+    case 8:
+      grade = 'B';
+      break;
+    case 7:
+      grade = 'C';
+      break;
+    case 6:
+      grade = 'D';
+      break;
+    default:
+      grade = 'F';
+    }
+    System.out.println(score + "점은 " + grade + "학점입니다.");
   }
   
   public static void ex03() {
@@ -84,7 +101,8 @@ public class MainWrapper {
   }
   
   public static void main(String[] args) {
-    ex01();
+    // ex01();
+    ex02();
   }
 
 }
