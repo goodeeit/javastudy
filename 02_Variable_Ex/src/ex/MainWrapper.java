30677cbdabbdb97acfad524bc13431737a482ff4
goodeeit <gt_min@naver.com>
2023-07-13 10:03:57 +0900

    20230713

---
+++ 02_Variable_Ex/src/ex/MainWrapper.java
@@ -0,0 +1,44 @@
+package ex;
+
+public class MainWrapper {
+
+  public static void main(String[] args) {
+    
+    
+    // 초 -> 분/초
+    int second = 90;
+    int m = ??;  // 1
+    int s = ??;  // 30
+
+    
+    // x와 y값을 교환
+    int x = 10;
+    int y = 20;
+    int temp;
+
+    
+    // 5% 이자는 얼마?(소수 이하는 버림)
+    long balance = 123456L;  // 통장잔액
+    double pct = 0.05;  // 5%
+
+    
+    // 90점대 점수이면 true, 아니면 false
+    int score1 = 95;
+    double result1 = ??;  // true
+    int score2 = 75;
+    double result2 = ??;  // false
+
+
+    // n의 "짝수", "홀수" 여부 출력
+    int n = 3;
+    String res1 = ??;  // "홀수"
+
+
+    // num의 "짝수", "홀수", "3의배수" 여부 출력
+    int num = 3;
+    String res2 = ??;  // "3의배수"
+    
+
+  }
+
+}
