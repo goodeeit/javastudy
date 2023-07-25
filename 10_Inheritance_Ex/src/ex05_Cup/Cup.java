cf5f33f32157340c15140e1e2c45d8be594d72a7
goodeeit <gt_min@naver.com>
2023-07-25 12:44:21 +0900

    20230725

---
+++ 10_Inheritance_Ex/src/ex05_Cup/Cup.java
@@ -0,0 +1,14 @@
+package ex05_Cup;
+
+public class Cup {
+
+  private Coffee coffee;
+
+  public Coffee getCoffee() {
+    return coffee;
+  }
+  public void setCoffee(Coffee coffee) {
+    this.coffee = coffee;
+  }
+  
+}
