33e2ef2d41f8fdcd7959bcd0f72b319ef63e25db
goodeeit <gt_min@naver.com>
2023-07-26 15:47:11 +0900

    20230726

--- 10_Inheritance_Ex/src/ex07_TourGuide/MainWrapper.java
+++ 10_Inheritance_Ex/src/ex07_TourGuide/MainWrapper.java
@@ -5,7 +5,7 @@ public class MainWrapper {
   public static void main(String[] args) {
     
     TourGuide guide1 = new TourGuide();
-    guide1.setTour(new HawaiiTour());
+    guide1.setTour(new HawaiiTour());  // public void setTour(Tour tour) { }   Tour tour = new HawaiiTour();
     guide1.sightseeing();
     guide1.leisure();
     guide1.meal();
