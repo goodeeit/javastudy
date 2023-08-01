4ebe155585cf8b03471c462ed12ea0ebdf488bed
goodeeit <gt_min@naver.com>
2023-08-01 15:22:13 +0900

    20230801

--- 16_Exception_Ex/src/ex01_BankAccount/MainWrapper.java
+++ 16_Exception_Ex/src/ex01_BankAccount/MainWrapper.java
@@ -4,9 +4,20 @@ public class MainWrapper {
 
   public static void main(String[] args) {
     
-    BankAccount acc = new BankAccount(10000, "1234");
-    long money = acc.withdrawal(5000);
-    System.out.println("출금액 " + money);
+    // deposit, withdrawal, transfer 메소드에서 던진 RuntimeException을
+    // 여기서 처리한다.
+    
+    try {
+      BankAccount acc = new BankAccount(10000, "1234");
+      BankAccount acc2 = new BankAccount(10000, "5678");
+      acc.deposit(10000);
+      acc2.withdrawal(5000);
+      acc.transfer(acc2, 1);
+      acc.inquiry();
+      acc2.inquiry();
+    } catch(RuntimeException e) {
+      System.out.println(e.getMessage());
+    }
     
   }
 
