50e041b40ec6c97258f5c69f93d1eb61810dc310
goodeeit <gt_min@naver.com>
2023-08-01 17:30:58 +0900

    20230801

--- 16_Exception_Ex/src/ex04_Library/Library.java
+++ 16_Exception_Ex/src/ex04_Library/Library.java
@@ -1,6 +1,7 @@
 package ex04_Library;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,10 +19,10 @@ public class Library {
   /**
    * 등록<br>
    */
-  private void addBook() {
+  private void addBook() throws InputMismatchException {
     System.out.println("=== 등록 ===");
     System.out.println("책 제목 입력");
-    String title = sc.nextLine();
+    String title = sc.next();
     System.out.println("책 가격 입력");
     int price = sc.nextInt();
     books.add(new Book(title, price));
@@ -52,8 +53,16 @@ public class Library {
   /**
    * 전체조회<br>
    */
-  private void queryAllBook() {
-    
+  private void queryAllBook() throws RuntimeException {
+    System.out.println("=== 전체조회 ===");
+    int size = books.size();
+    System.out.println("전체 보유 도서: " + size + "권");
+    if(books.isEmpty()) {
+      throw new RuntimeException("도서관에 등록된 책이 없습니다.");
+    }
+    for(int i = 0; i < size; i++) {
+      System.out.println((i + 1) + "번째 도서: " + books.get(i));
+    }
   }
   
   /**
@@ -64,21 +73,27 @@ public class Library {
     // 사용자가 종료 명령을 내릴때까지 프로그램은 종료되지 않는다.
     while(true) {
       
-      System.out.println("1.등록 2.삭제 3.수정 4.조회 5.전체조회 0.종료");
-      String choice = sc.next();
+      try {
       
-      switch(choice) {
-      case "1": addBook(); break;
-      case "2": deleteBook(); break;
-      case "3": modifyBook(); break;
-      case "4": queryBook(); break;
-      case "5": queryAllBook(); break;
-      case "0": System.out.println("도서관리프로그램을 종료합니다."); return;
-      default: System.out.println("잘못된 입력입니다. 다시 시도하세요.");
+        System.out.println("1.등록 2.삭제 3.수정 4.조회 5.전체조회 0.종료");
+        String choice = sc.next();
+        
+        switch(choice) {
+        case "1": addBook(); break;
+        case "2": deleteBook(); break;
+        case "3": modifyBook(); break;
+        case "4": queryBook(); break;
+        case "5": queryAllBook(); break;
+        case "0": System.out.println("도서관리프로그램을 종료합니다."); return;
+        default: System.out.println("잘못된 입력입니다. 다시 시도하세요.");
+        }
+        
+      } catch(Exception e) {
+        System.out.println(e.getMessage());
       }
       
-    }
+    }  // while(true)
     
-  }
+  }  // manage()
   
 }
