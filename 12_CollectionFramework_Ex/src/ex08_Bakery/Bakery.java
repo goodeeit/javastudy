c8fd04c6b9c56afef2807327c8e1b38698314530
goodeeit <gt_min@naver.com>
2023-08-01 15:45:47 +0900

    20230801

--- 12_CollectionFramework_Ex/src/ex08_Bakery/Bakery.java
+++ 12_CollectionFramework_Ex/src/ex08_Bakery/Bakery.java
@@ -39,6 +39,12 @@ public class Bakery {
    */
   public Map<String, Integer> sell(int count, int money) {
     
+    // 0 이하의 빵을 요청했다.
+    if(count <= 0) {
+      System.out.println("판매 불가합니다.(0 이하의 빵을 요청하다.)");
+      return null;
+    }
+    
     // 빵이 부족하다.
     if(this.count < count) {
       System.out.println("판매 불가합니다.(빵이 부족하다.)");
