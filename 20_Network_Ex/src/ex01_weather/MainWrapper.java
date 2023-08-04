9671ddc3289126822e0eaad706e7cd66879a59ce
goodeeit <gt_min@naver.com>
2023-08-04 15:00:58 +0900

    20230804

--- 20_Network_Ex/src/ex01_weather/MainWrapper.java
+++ 20_Network_Ex/src/ex01_weather/MainWrapper.java
@@ -7,6 +7,8 @@ public class MainWrapper {
     // 1시간마다 갱신되는 전국 날씨 정보
     String spec = "http://www.kma.go.kr/XML/weather/sfc_web_map.xml";
 
+    // C:/storage/sfc_web_map.xml 파일로 저장하기
+    
   }
 
 }
