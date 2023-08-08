86df7f6abc12e141d356ed8f20b398ada1a5dd5b
goodeeit <gt_min@naver.com>
2023-08-08 12:39:20 +0900

    20230808

--- 19_Network/src/ex02_api/NaverCaptcha.java
+++ 19_Network/src/ex02_api/NaverCaptcha.java
@@ -1,6 +1,10 @@
 package ex02_api;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -64,11 +68,64 @@ public class NaverCaptcha {
     
   }
   
-  public static void main(String[] args) {
+  private static void getImage() {
     
-    String key = getKey();
-    System.out.println("캡차키: " + key);
-
+    URL url = null;
+    HttpURLConnection con = null;
+    BufferedInputStream bin = null;
+    BufferedOutputStream bout = null;
+    
+    try {
+      
+      String key = getKey();
+      String spec = "https://openapi.naver.com/v1/captcha/ncaptcha.bin?key=" + key;
+      
+      url = new URL(spec);
+      con = (HttpURLConnection) url.openConnection();
+      
+      con.setRequestMethod("GET");
+      
+      con.setRequestProperty("X-Naver-Client-Id", CLIENT_ID);
+      con.setRequestProperty("X-Naver-Client-Secret", CLIENT_SECRET);
+      
+      int responseCode = con.getResponseCode();
+      if(responseCode != HttpURLConnection.HTTP_OK) {
+        throw new RuntimeException(responseCode + " 발생");
+      }
+      
+      bin = new BufferedInputStream(con.getInputStream());
+      
+      File dir = new File("C:/storage");
+      if(dir.exists() == false) {
+        dir.mkdirs();
+      }
+      File file = new File(dir, System.currentTimeMillis() + ".jpg");
+      bout = new BufferedOutputStream(new FileOutputStream(file));
+      
+      byte[] b = new byte[1024];
+      int readByte = 0;
+      while((readByte = bin.read(b)) != -1) {
+        bout.write(b, 0, readByte);
+      }
+      
+      System.out.println(file.getPath() + " 파일 생성 완료");
+      
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    } finally {
+      try {
+        if(bout != null) bout.close();
+        if(bin != null) bin.close();
+        if(con != null) con.disconnect();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    
+  }
+  
+  public static void main(String[] args) {
+    getImage();
   }
 
 }
