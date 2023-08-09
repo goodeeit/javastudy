3957a140591ef2e10948bd86035b524077771f83
goodeeit <gt_min@naver.com>
2023-08-09 16:13:40 +0900

    20230809

--- 21_Jdbc/src/ex05_select/Ex02_selectList.java
+++ 21_Jdbc/src/ex05_select/Ex02_selectList.java
@@ -3,8 +3,11 @@ package ex05_select;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import connect.DB_Connect;
+import dto.UserDto;
 
 public class Ex02_selectList {
 
@@ -30,6 +33,7 @@ public class Ex02_selectList {
       String sql = "";
       sql += "SELECT USER_NO, USER_ID, USER_NAME, JOINED_AT";
       sql += "  FROM USER_T";
+      sql += " ORDER BY USER_NO";
       
       // PreparedStatement 객체 생성
       ps = con.prepareStatement(sql);
@@ -37,7 +41,28 @@ public class Ex02_selectList {
       // 쿼리문 실행
       rs = ps.executeQuery();
       
+      // 검색 결과를 저장할 List<UserDto> 생성
+      List<UserDto> users = new ArrayList<UserDto>();
       
+      // 검색 결과가 여러 개인 경우 while문을 이용해서 검색 결과가 있는지 여러 번 체크한다.
+      while(rs.next()) {
+        
+        // 검색 결과 행(Row) 1개 → UserDto 객체 생성
+        UserDto user = new UserDto();
+        user.setUser_no(rs.getInt(1));
+        user.setUser_id(rs.getString(2));
+        user.setUser_name(rs.getString(3));
+        user.setJoined_at(rs.getDate(4));
+        
+        // UserDto 객체 -> List<UserDto> 추가
+        users.add(user);
+        
+      }
+      
+      // 검색 결과 확인
+      for(UserDto user : users) {
+        System.out.println(user);
+      }
       
     } catch (Exception e) {
       e.printStackTrace();
