7ded7e108d151776746a6bde342fadfd40106ab5
goodeeit <gt_min@naver.com>
2023-08-09 16:31:07 +0900

    20230809

--- 21_Jdbc/src/ex03_update/MainWrapper.java
+++ 21_Jdbc/src/ex03_update/MainWrapper.java
@@ -1,4 +1,4 @@
-package ex02_insert;
+package ex03_update;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,18 +11,18 @@ public class MainWrapper {
 
   public static void main(String[] args) {
   
-    // 삽입할 User 정보 입력
+    // 수정할 User 정보 입력
     Scanner sc = new Scanner(System.in);
-    System.out.print("USER_ID >>> ");
-    String user_id = sc.next();
+    System.out.print("수정할 USER_NO >>> ");
+    int user_no = sc.nextInt();
     sc.nextLine();
-    System.out.print("USER_NAME >>> ");
+    System.out.print("수정할 USER_NAME >>> ");
     String user_name = sc.nextLine();
     sc.close();
     
     // UserDto 객체 생성
     UserDto user = new UserDto();
-    user.setUser_id(user_id);
+    user.setUser_no(user_no);
     user.setUser_name(user_name);
     
     // Connection 객체 선언 (DB 접속)
@@ -38,21 +38,22 @@ public class MainWrapper {
       
       // 쿼리문
       String sql = "";
-      sql += "INSERT INTO USER_T (USER_NO, USER_ID, USER_NAME, JOINED_AT) ";
-      sql += "VALUES(USER_SEQ.NEXTVAL, ?, ?, SYSDATE)";
+      sql += "UPDATE USER_T";
+      sql += "   SET USER_NAME = ?";
+      sql += " WHERE USER_NO = ?";
       
       // PreparedStatement 객체 생성
       ps = con.prepareStatement(sql);
       
       // 쿼리문에 변수 넣기
-      ps.setString(1, user.getUser_id());   // 1번째 물음표 ← user.getUser_id()
-      ps.setString(2, user.getUser_name()); // 2번째 물음표 ← user.getUser_name()
+      ps.setString(1, user.getUser_name()); // 1번째 물음표 ← user.getUser_name()
+      ps.setInt(2, user.getUser_no());      // 2번째 물음표 ← user.getUser_no()
       
-      // 쿼리문 실행 : insert 된 행의 개수가 반환된다.
-      int insertResult = ps.executeUpdate();
+      // 쿼리문 실행 : update 된 행의 개수가 반환된다.
+      int updateResult = ps.executeUpdate();
       
       // 결과
-      System.out.println(insertResult + "행이 삽입되었습니다.");
+      System.out.println(updateResult + "행이 수정되었습니다.");
       
       // 커밋? 안 한다.
       // con.setAutoCommit(true); ← 기본값으로 사용되고 있다.
--- 21_Jdbc/src/ex04_delete/MainWrapper.java
+++ 21_Jdbc/src/ex04_delete/MainWrapper.java
@@ -1,29 +1,20 @@
-package ex02_insert;
+package ex04_delete;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.Scanner;
 
 import connect.DB_Connect;
-import dto.UserDto;
 
 public class MainWrapper {
 
   public static void main(String[] args) {
   
-    // 삽입할 User 정보 입력
+    // 삭제할 USER_NO 정보 입력
     Scanner sc = new Scanner(System.in);
-    System.out.print("USER_ID >>> ");
-    String user_id = sc.next();
-    sc.nextLine();
-    System.out.print("USER_NAME >>> ");
-    String user_name = sc.nextLine();
+    System.out.print("삭제할 USER_NO >>> ");
+    int user_no = sc.nextInt();
     sc.close();
-    
-    // UserDto 객체 생성
-    UserDto user = new UserDto();
-    user.setUser_id(user_id);
-    user.setUser_name(user_name);
     
     // Connection 객체 선언 (DB 접속)
     Connection con = null;
@@ -38,21 +29,21 @@ public class MainWrapper {
       
       // 쿼리문
       String sql = "";
-      sql += "INSERT INTO USER_T (USER_NO, USER_ID, USER_NAME, JOINED_AT) ";
-      sql += "VALUES(USER_SEQ.NEXTVAL, ?, ?, SYSDATE)";
+      sql += "DELETE";
+      sql += "  FROM USER_T";
+      sql += " WHERE USER_NO = ?";
       
       // PreparedStatement 객체 생성
       ps = con.prepareStatement(sql);
       
       // 쿼리문에 변수 넣기
-      ps.setString(1, user.getUser_id());   // 1번째 물음표 ← user.getUser_id()
-      ps.setString(2, user.getUser_name()); // 2번째 물음표 ← user.getUser_name()
+      ps.setInt(1, user_no);   // 1번째 물음표 ← user_no
       
-      // 쿼리문 실행 : insert 된 행의 개수가 반환된다.
-      int insertResult = ps.executeUpdate();
+      // 쿼리문 실행 : delete 된 행의 개수가 반환된다.
+      int deleteResult = ps.executeUpdate();
       
       // 결과
-      System.out.println(insertResult + "행이 삽입되었습니다.");
+      System.out.println(deleteResult + "행이 삭제되었습니다.");
       
       // 커밋? 안 한다.
       // con.setAutoCommit(true); ← 기본값으로 사용되고 있다.
