38a9b93dd403528edb756c94b8f1c639a299969e
goodeeit <gt_min@naver.com>
2023-08-11 09:04:48 +0900

    20230811

--- 14_Java_Api_Ex/src/ex04_Lotto/Lotto.java
+++ 14_Java_Api_Ex/src/ex04_Lotto/Lotto.java
@@ -17,16 +17,7 @@ public class Lotto {
    * @return 0 로또 구매 실패 시 반환
    */
   public int buyLotto() {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Lotto를 얼마나 구입하시겠습니까?(최대 10만원) >>> ");
-    int money = sc.nextInt();
-    if(money < 1000 || money > 100000) {
-      System.out.println(money + "원 Lotto 구매는 불가능합니다.");
-      sc.close();
-      return 0;
-    }
-    sc.close();
-    return money /1000 * 1000;
+    return 0;
   }
   
   /**
@@ -36,30 +27,7 @@ public class Lotto {
    * @param money 로또 구매 비용
    */
   public void generateLotto(int money) {
-    while(money > 0) {
-      int row = (money >= 5000) ? 5 : money / 1000;
-      int[][] lotto = new int[row][6];
-      for (int i = 0; i < lotto.length; i++) {
-        Random random = new Random();
-        Set<Integer> set = new HashSet<Integer>();
-        while(set.size() != 6) {
-          set.add(random.nextInt(45) + 1);
-        }
-        List<Integer> list = new ArrayList<Integer>(set);
-        for(int j = 0; j < lotto[i].length; j++) {
-          lotto[i][j] = list.get(j);
-        }
-      }
-      for (int i = 0; i < lotto.length; i++) {
-        System.out.print(String.format("%02d", i + 1) + " : ");
-        for (int j = 0; j < lotto[i].length; j++) {
-          System.out.print( String.format("%4d", lotto[i][j]) );
-        }
-        System.out.println();
-      }
-      System.out.println("------------------------------");
-      money -= 5000;
-    }
+    
   }
   
 }
